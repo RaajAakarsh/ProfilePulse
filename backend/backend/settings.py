9b5fa8d09b68ae28896360189c9eacb84493e03f
RaajAakarsh <aakarshraaj032@gmail.com>
2024-03-11 17:29:13 +0530

    made some minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,10 +95,10 @@ AUTH_USER_MODEL = "users.User"
 
 # EMAIL CONFIG
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_FROM_USER = 'sampletesting662@gmail.com'
+EMAIL_FROM_USER = 'YOUR_EMAIL'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sampletesting662@gmail.com'
-EMAIL_HOST_PASSWORD = 'cygdgmtkejxwbifz'
+EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
 EMAIL_USE_TLS = True
 EMAIL_PORT =587
 
